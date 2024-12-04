52437443a2be6be3f603531429eb71ce978370dd
Hesch <heschmat.seyedi@gmail.com>
2024-12-05 00:29:23 +0100

    Initialize Setup TF

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.23.0"
     }
   }
 
   backend "s3" {
-    bucket = "udemy-devops-tf"
-    key = "tf-state-setup"
-    region = "us-east-1"
-    encrypt = true
+    bucket         = "udemy-devops-tf"
+    key            = "tf-state-setup"
+    region         = "eu-central-1"
+    encrypt        = true
     dynamodb_table = "udemy-devops-tf-lock"
   }
 }
@@ -21,9 +21,9 @@ provider "aws" {
   default_tags {
     tags = {
       Environment = terraform.workspace
-      Project = var.project
-      contact = var.contact
-      ManageBy = "Terraform/setup"
+      Project     = var.project
+      contact     = var.contact
+      ManageBy    = "Terraform/setup"
     }
   }
 }
--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,19 +1,19 @@
 variable "tf_state_bucket" {
-    description = "AWS S3 Bucket name for storing TF state"
-    default = "udemy-devops-tf-"
+  description = "AWS S3 Bucket name for storing TF state"
+  default     = "udemy-devops-tf-"
 }
 
 variable "tf_state_lock_table" {
-    description = "AWS DynamoDB table name for TF state locking"
-    default = "udemy-devops-tf-lock"
+  description = "AWS DynamoDB table name for TF state locking"
+  default     = "udemy-devops-tf-lock"
 }
 
 variable "project" {
-    description = "Project name for tagging resources"
-    default = "drf_recipe_app"
+  description = "Project name for tagging resources"
+  default     = "drf_recipe_app"
 }
 
 variable "contact" {
-    description = "Contact name for tagging resources"
-    default = "admin@example.com"
+  description = "Contact name for tagging resources"
+  default     = "admin@example.com"
 }
